1c53232beef54f5c9abf93cc5a04c2065ece015a
AliceChpmnk <alisablog50@gmail.com>
2023-03-22 23:54:53 +0200

    create slices

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
-import { createStore } from "redux";
-import { devToolsEnhancer } from "@redux-devtools/extension";
-import { rootReducer } from "./reducer";
+import { configureStore } from "@reduxjs/toolkit";
+import { tasksReducer } from "./tasksSlice";
+import { filtersReducer } from "./filtersSlice";
 
-// Створюємо розширення стора, щоб додати інструменти розробника
-// Якщо вам не потрібний початковий стан preloadedState, то значення enhancer передається другим аргументом. В іншому випадку - третім.
-const enhancer = devToolsEnhancer();
-export const store = createStore(rootReducer, enhancer);
+export const store = configureStore({
+  reducer: {
+    tasks: tasksReducer,
+    filters: filtersReducer,
+  },
+});
